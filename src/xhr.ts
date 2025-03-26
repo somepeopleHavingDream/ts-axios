804f4e3437f7ea4ab835a469e85f73aa1c64de77
yangxin <1393003255@qq.com>
2025-03-26 18:05:49 +0800

    feat: ts-axios 基础功能实现【核心流程】，处理响应 header - 需求分析 + parseHeaders 函数实现及应用

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,4 +1,5 @@
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise(resolve => {
@@ -18,7 +19,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         return
       }
 
-      const responseHeaders = request.getAllResponseHeaders()
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
       const responseData = responseType !== 'text' ? request.response : request.responseText
       const response: AxiosResponse = {
         data: responseData,
