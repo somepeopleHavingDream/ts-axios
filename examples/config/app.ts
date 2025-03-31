466a9faeba097e161cb4ce48a39d30bae24bdd12
yangxin <1393003255@qq.com>
2025-03-31 23:13:59 +0800

    feat: ts-axios 配置化实现【巧妙运用策略模式实现配置合并】，扩展 axios.create 静态接口 - 需求分析 + 静态方法扩展 + demo 实现

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -16,7 +16,32 @@ import qs from 'qs'
 //   console.log(res.data)
 // })
 //
-axios({
+// axios({
+//   transformRequest: [
+//     function(data) {
+//       return qs.stringify(data)
+//     },
+//     ...(axios.defaults.transformRequest as AxiosTransformer[])
+//   ],
+//   transformResponse: [
+//     ...(axios.defaults.transformResponse as AxiosTransformer[]),
+//     function(data) {
+//       if (typeof data === 'object') {
+//         data.b = 2
+//       }
+//       return data
+//     }
+//   ],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then(res => {
+//   console.log(res.data)
+// })
+
+const instance = axios.create({
   transformRequest: [
     function(data) {
       return qs.stringify(data)
@@ -31,7 +56,10 @@ axios({
       }
       return data
     }
-  ],
+  ]
+})
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
@@ -40,25 +68,3 @@ axios({
 }).then(res => {
   console.log(res.data)
 })
-
-// const instance = axios.create({
-//   transformRequest: [(function(data) {
-//     return qs.stringify(data)
-//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
-//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
-//     if (typeof data === 'object') {
-//       data.b = 2
-//     }
-//     return data
-//   }]
-// })
-//
-// instance({
-//   url: '/config/post',
-//   method: 'post',
-//   data: {
-//     a: 1
-//   }
-// }).then((res) => {
-//   console.log(res.data)
-// })
--- src/axios.ts
+++ src/axios.ts
@@ -1,9 +1,10 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
 
   // `instance` 不仅是一个绑定了上下文的 `request` 方法，还包含 `Axios` 实例的所有其他方法和属性
@@ -11,9 +12,12 @@ function createInstance(config: AxiosRequestConfig): AxiosInstance {
   const instance = Axios.prototype.request.bind(context)
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
