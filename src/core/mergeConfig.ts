02f640a672ed5346c43004538e70354fd133a588
yangxin <1393003255@qq.com>
2025-03-31 17:03:36 +0800

    feat: ts-axios 配置化实现【巧妙运用策略模式实现配置合并】，合并配置的设计与实现 - 配置合并策略（上）

---
+++ src/core/mergeConfig.ts
@@ -0,0 +1,46 @@
+import { AxiosRequestConfig } from '../types'
+
+const strategies = Object.create(null)
+
+function defaultStrategy(val1: any, val2: any): any {
+  return typeof val2 !== 'undefined' ? val2 : val1
+}
+
+function fromVal2Strategy(val1: any, val2: any): any {
+  if (typeof val2 !== 'undefined') {
+    return val2
+  }
+}
+
+const strategyKeysFromVal2 = ['url', 'params', 'data']
+strategyKeysFromVal2.forEach(key => {
+  strategies[key] = fromVal2Strategy
+})
+
+export default function mergeConfig(
+  config1: AxiosRequestConfig,
+  config2?: AxiosRequestConfig
+): AxiosRequestConfig {
+  if (!config2) {
+    config2 = {}
+  }
+
+  const config = Object.create(null)
+
+  for (let key in config2) {
+    mergeField(key)
+  }
+
+  for (let key in config1) {
+    if (!config2[key]) {
+      mergeField(key)
+    }
+  }
+
+  function mergeField(key: string): void {
+    const strategy = strategies[key] || defaultStrategy
+    config[key] = strategy(config1[key], config2![key])
+  }
+
+  return config
+}
